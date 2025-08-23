d86c8a61f23be334c549c02ceda874cc6ac6b4a5
SrujanaEllur <srujanaellur@gmail.com>
2025-08-23 14:38:34 +0530

    srujana query.js to insert

--- query.js
+++ query.js
@@ -7,3 +7,18 @@
 //   email VARCHAR(100),
 //   mobile VARCHAR(15)
 // );
+
+
+//to INSERT data into the table
+// try {
+//     const query = `
+//       INSERT INTO input_output (name, age, email, mobile)
+//       VALUES ($1, $2, $3, $4)
+//     `;
+//     await pool.query(query, [name, age, email, mobile]);
+//     res.send('User added successfully!');
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send('Database error');
+//   }
+// });
