4778c2977db3c7cec14a4c5c7aa71854bd67d4d2
Bhoomika Suresh <bhoomikasureshg@gmail.com>
2025-08-24 13:02:33 +0530

    new form for submiting and displaying the user data

--- add.js
+++ add.js
@@ -5,7 +5,7 @@ import path from "path";
 import { fileURLToPath } from "url"; 
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const _dirname = path.dirname(_filename);
 
 
 const app = express();
